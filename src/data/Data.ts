5f72e97507d5d4872553dab6fb756002da7179e4
Marcos Morales <mmorales@orangeloops.com>
2024-04-04 15:09:56 -0300

    Implement the task list

---
+++ src/data/Data.ts
@@ -0,0 +1,27 @@
+export interface Task {
+    id: number;
+    name: string;
+    detail: string;
+    completed: boolean;
+}
+
+export const tasks: Task[] = [
+    {
+        id: 0,
+        name: "Learn TypeScript",
+        detail: "Detail for Learn TypeScript",
+        completed: false
+    },
+    {
+        id: 1,
+        name: "Build a to-do list app",
+        detail: "Detail for Build a to-do list app",
+        completed: false
+    },
+    {
+        id: 2,
+        name: "Practice coding",
+        detail: "Detail for Practice coding",
+        completed: false
+    }
+];
